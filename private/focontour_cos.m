1f39a430ae2f0ce64e91444ba305a6db56eee10f
hasinirw <hasiniw@bu.edu>
2021-11-08 17:39:25 -0500

    changed LeDIVA to branch

--- private/focontour_cos.m
+++
@@ -1,7 +0,0 @@
-function fo = focontour_cos(f01,f02,N)
-
-A = (f01-f02)/2;
-B = (f01+f02)/2;
-
-fo = A*cos(2*pi*[0:N-1]/(2*N)) + B;
-
--- private/pushbutton2_Callback.m
+++
@@ -1,4 +0,0 @@
-%pushbutton2_Callback.m
-%B. Story
-clf
-clear hx02r hx02l hxibr hxibl hnpr hnpl haepi hlepi hpgap hvow1 hvow2 hdxnew htd hfo h1o h p c
